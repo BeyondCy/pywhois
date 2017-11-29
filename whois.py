a1f2a3ce99447a55056148d9e7f41920bf819394
fen <felicitychou@users.noreply.github.com>
2017-11-29 20:17:20 +0800

    update whois.py

--- whois.py
+++ whois.py
@@ -6,23 +6,27 @@ import re
 import chardet
  
 def send(keyword,server='whois.iana.org',port=43):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((server, port))
-    s.send(b'%s\r\n' % bytes(keyword, encoding='utf-8'))
 
-    buffer = []
-    while True:
-        d = s.recv(1024)
-        if d:
-            buffer.append(d)
-        else:
-            break        
-    data = b''.join(buffer)
-    print(chardet.detect(data)['encoding'])
-    return str(data,encoding=chardet.detect(data)['encoding'])
+   with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((server, port))
+        s.send(b'%s\r\n' % bytes(keyword, encoding='utf-8'))
+
+        buffer = []
+        while True:
+            d = s.recv(1024)
+            if d:
+                buffer.append(d)
+            else:
+                break        
+        data = b''.join(buffer)
+        return data
 
 def query(keyword):
     info  = send(keyword=keyword)
-    whois_server = re.search(r'whois:\s*([A-Za-z0-9\_\-\.]+)',info).group(1)
-    info  = send(keyword=keyword,server=whois_server)
-    return info
+    temp = re.search(r'whois:\s*([A-Za-z0-9\_\-\.]+)',str(info,encoding=chardet.detect(info)['encoding']))
+    if temp:
+        whois_server = temp.group(1)
+        info  = send(keyword=keyword,server=whois_server)
+        return info
+    else:
+        return info
