3cf3cf7fd47c0bd191c42cb667ad6b96a209f9c3
fen <felicitychou@users.noreply.github.com>
2017-12-15 09:43:45 +0800

    update update.py

--- update.py
+++ update.py
@@ -2,6 +2,7 @@ import requests,re,os
 from urllib.parse import urljoin
 import csv
 import configparser
+from io import StringIO
 
 CONFIG = configparser.ConfigParser()
 CONFIG['domain'] = {}
@@ -10,65 +11,47 @@ CACHE_PATH = 'cache'
 if not os.path.exists(CACHE_PATH):
     os.mkdir(CACHE_PATH)
 
-def cache(filename,url):
+def download(filename,url):
     data = None
     if CACHE_PATH and os.path.exists(CACHE_PATH) and os.path.isdir(CACHE_PATH):
         filepath = os.path.join(CACHE_PATH,filename)
-        if not os.path.exists(filepath):
+        if os.path.exists(filepath):
+            with open(filepath,'rb') as fr:
+                data = fr.read()
+        else:
             r = requests.get(url)
             data = r.content
             with open(filepath,'wb') as fw:
                 fw.write(r.content)
-        else:
-            with open(filepath,'wb') as fr:
-                data = fr.read()
+    else:
+        r = requests.get(url)
+        data = r.content
+
     return data
 
 
 def update():
     # domain
-    base_url = 'https://www.iana.org/'
-
-
-    data = cache()
-    
-    if CACHE_PATH and os.path.exists(CACHE_PATH) and os.path.isdir(CACHE_PATH):
-        ROOT_DB = os.path.join(CACHE_PATH,'root_db.html')
-        if not os.path.exists(ROOT_DB):
-            r = requests.get('https://www.iana.org/domains/root/db')
-            with open(ROOT_DB,'wb') as fw:
-                fw.write(r.content)
-                data = r.content
-        else:
-            with open(ROOT_DB,'wb') as fr:
-                data = fr.read()
-
+    data = download(filename='root_db.html',url='https://www.iana.org/domains/root/db')
     result = re.findall('(\/domains\/root\/db\/.+?\.html)\">(.+?)<',str(data, encoding='utf-8'))
     for item in result:
         # 
         if 'xn--' in item[0]:
             continue
 
-        TLD_URL = urljoin(base_url,item[0])
-        TLD_FILE = os.path.join(CACHE_PATH,os.path.basename(TLD_URL))
-        if not os.path.exists(TLD_FILE):
-            r = requests.get(TLD_URL)
+        TLD_URL = urljoin('https://www.iana.org/',item[0])
+        data = download(filename=os.path.basename(TLD_URL),url=TLD_URL)
 
-        if CACHE_PATH:
-            with open(TLD_FILE,'wb') as fw:
-                fw.write(r.content)
+        whois = re.search('WHOIS Server:</b> (\S+)',str(data, encoding='utf-8'))
+        CONFIG['domain'][item[1][1:]] = whois[1] if whois else ''
 
-        whois = re.search('WHOIS Server:</b> (\S+)',str(r.content, encoding='utf-8'))
-        if whois:
-            CONFIG['domain'][item[1][1:]] = whois[1]
-        else:
-            CONFIG['domain'][item[1][1:]] = ''
         print('Update %s whois server' % (item[1][1:],))
 
     # ip
     # https://www.iana.org/assignments/ipv4-address-space/ipv4-address-space.csv
-    r = requests.get('https://www.iana.org/assignments/ipv4-address-space/ipv4-address-space.csv')
-    reader = csv.DictReader(StringIO(str(r.content, encoding='utf-8')))
+    IPv4_URL = 'https://www.iana.org/assignments/ipv4-address-space/ipv4-address-space.csv'
+    data = download(filename=os.path.basename(IPv4_URL),url=IPv4_URL)
+    reader = csv.DictReader(StringIO(str(data, encoding='utf-8')))
     for row in reader:
         CONFIG['ip'][str(int(row['Prefix'].split('/')[0]))] = row['WHOIS']
         print('Update %s whois server' % (row['Prefix'],))
